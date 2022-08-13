9535e978677afb7457575cd9e8b10134fe0b1258
Rodolfo Montes <rodolfo.montes@a7.net.br>
2022-08-13 11:51:15 -0300

    User entity and resource

---
+++ src/main/java/com/montes/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.montes.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.montes.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "maria", "maria@gmail.com", "99999999", "123");
+		return ResponseEntity.ok().body(u);
+	}
+}
