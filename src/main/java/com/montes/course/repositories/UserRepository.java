3d39146dd15c5a993a1e094d284365bff15a6c4a
Rodolfo Montes <rodolfo.montes@a7.net.br>
2022-08-13 12:41:19 -0300

    Service layer, component registration

--- src/main/java/com/montes/course/repositories/UserRepository.java
+++ src/main/java/com/montes/course/repositories/UserRepository.java
@@ -1,8 +1,11 @@
 package com.montes.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.montes.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	
 }
---
+++ src/main/java/com/montes/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.montes.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.montes.course.entities.User;
+import com.montes.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
