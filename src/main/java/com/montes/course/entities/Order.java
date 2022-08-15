0675821bb5b58e988ea2634b6c823424ab1442b6
Rodolfo Montes <rodolfo.montes@a7.net.br>
2022-08-15 19:36:54 -0300

    Subtotal & Total methods

--- src/main/java/com/montes/course/entities/Order.java
+++ src/main/java/com/montes/course/entities/Order.java
@@ -96,7 +96,14 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
-
+	
+	public Double getTotal() {
+		double sum = 0;
+		for (OrderItem orderItem : items) {
+			sum += orderItem.getSubTotal();
+		}
+		return sum;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
